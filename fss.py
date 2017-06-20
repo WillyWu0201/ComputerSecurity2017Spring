7cad52902cbf3773946931049f63e5c5f3ec725d
Wu, Chia-Wei <willy@loftechs.com>
2017-06-21 00:32:19 +0800

    Update fss_initialize

--- fss.py
+++ fss.py
@@ -1,7 +1,9 @@
-PrfKeys = [[]]
+import numpy
+
+PrfKeys = numpy.zeros((4,16),int)
 FixedBlocks = []
 M = 0
 N = 0
 NumBits = 0
-Temp = []
-Out = []
+Temp = numpy.zeros((1,16),int)
+Out = numpy.zeros((1,16 * 4),int)
---
+++ fss_initialize.py
@@ -0,0 +1,53 @@
+import fss
+import numpy
+from numpy import ndarray
+
+def ClientInitialize(numBits):
+    f = fss
+    f.NumBits = numBits
+    for i in range(0, 4, 1):
+        f.PrfKeys[i] = numpy.random.randint(256, size=16)
+        # 以下註解是go語言 還沒改成python
+        # // 產生加密用的block
+        # block, err: = aes.NewCipher(f.PrfKeys[i])
+        # if err != nil {
+        # panic(err.Error())
+        # }
+        # f.FixedBlocks[i] = block
+
+    # Check if int is 32 or 64 bit
+    x = numpy.uint64(1 << 32)
+    if numpy.uint(x) == 0:
+        f.N = 32
+    else:
+        f.N = 64
+    f.M = 4
+    return f
+
+
+def ServerInitialize(prfKeys, numBits):
+    f = fss
+    f.NumBits = numBits
+    for i in range(0, 4, 1):
+        f.PrfKeys[i] = prfKeys[i]
+        # 以下註解是go語言 還沒改成python
+        # // 產生加密用的block
+        # block, err: = aes.NewCipher(f.PrfKeys[i])
+        # if err != nil {
+        # panic(err.Error())
+        # }
+        # f.FixedBlocks[i] = block
+    # Check if int is 32 or 64 bit
+    x = numpy.uint64(1 << 32)
+    if numpy.uint(x) == 0:
+        f.N = 32
+    else:
+        f.N = 64
+    f.M = 4
+    return f
+
+# 測試function功能是否pass
+a = ClientInitialize(4)
+print(a.PrfKeys)
+b = ServerInitialize(a.PrfKeys, 4)
+print(b.PrfKeys)
