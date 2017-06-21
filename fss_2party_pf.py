c1dcdee8bb9a41c963742a9ea944d2b72536d83a
廖健智 <blue21423@gmail.com>
2017-06-22 01:50:29 +0800

    update fss_2party_pf.py

--- fss_2party_pf.py
+++ fss_2party_pf.py
@@ -1,4 +1,19 @@
 import binary
 from Crypto.Cipher import AES
+import FssKeyEq2P
+import os
 
 def GenerateTreePF(a, b):
+    fssKeys = FssKeyEq2P[2]
+
+    tempRand1 = bytes[AES.BlockSize + 1]
+
+    os.urandom().Read(tempRand1)
+
+    fssKeys[0].SInit = tempRand1[:AES.BlockSize]
+
+    fssKeys[0].TInit = tempRand1[AES.BlockSize] % 2
+
+    fssKeys[1].SInit = bytes[AES.BlockSize]
+
+    os.urandom().Read(fssKeys[1].SInit)
