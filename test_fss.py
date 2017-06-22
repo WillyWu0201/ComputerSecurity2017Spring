b89a8a93f288ee5e109e7ef5d3d313cd3d88c037
LAPTOP-CPD2IF36\user <nchu.si.j@gmail.com>
2017-06-22 23:17:09 +0800

    test_fss

---
+++ test_fss.py
@@ -0,0 +1,35 @@
+
+import fss_initialize
+
+#Generate fss Keys on client
+#fClient's format = type Fss struct { @fss_util
+fClient = fss_initialize.ClientInitialize(6)
+
+
+#Test with if x = 5, evaluate to 1
+
+fssKeys = fClient.GenerateTreePF(5, 1)
+
+#Simulate server
+
+fServer = fss_initialize.ServerInitialize(fClient.PrfKeys, fClient.NumBits)
+
+#Test 2-party Equality Function
+ans0 = 0
+ans1 = 0
+
+ans0 = fServer.EvaluatePF(0, fssKeys[0], 5)
+ans1 = fServer.EvaluatePF(1, fssKeys[1], 5)
+
+
+print("===============")
+print("ans0:", ans0)
+print("ans1:", ans1)
+print("Match (should be non-zero):", ans0+ans1)
+
+ans0 = fServer.EvaluatePF(0, fssKeys[0], 11)
+ans1 = fServer.EvaluatePF(1, fssKeys[1], 11)
+print("===============")
+print("ans0:", ans0)
+print("ans1:", ans1)
+print("No Match (should be 0):", ans0+ans1)
